e4c1ab544ae6dcf767f1312ade429ee6fd5b4a1b
hugosmoreira <hugo.n4@gmail.com>
2021-11-28 20:11:04 -0800

    update slug.js

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,4 +1,19 @@
+const { events } = require('./data.json')
+
+
+
 export default function EventPage() {
+
+    const evt = events.filter(ev => ev.slug === req.query.slug)
+
+    if(req.method === 'GET') {
+        res.status(200).json(evt)
+    } else {
+        res.setHeader('Allow', ['GET'])
+        res.status(405).end(`Method ${req.method} not allowed`)
+    }
+
+
     return (
         <div>
             <h1>My Event</h1>
